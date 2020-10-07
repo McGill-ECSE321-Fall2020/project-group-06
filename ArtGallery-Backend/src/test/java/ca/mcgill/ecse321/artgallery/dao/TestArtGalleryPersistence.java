ffe89b17b9d544ecd053200133f939b1d7ea18b9
Andre-Walter Panzini <aw.panzini@gmail.com>
2020-10-07 17:45:55 -0400

    Added persistence test for artistRepo

--- ArtGallery-Backend/src/test/java/ca/mcgill/ecse321/artgallery/dao/TestArtGalleryPersistence.java
+++ ArtGallery-Backend/src/test/java/ca/mcgill/ecse321/artgallery/dao/TestArtGalleryPersistence.java
@@ -84,16 +84,49 @@ public class TestArtGalleryPersistence {
 	}
 
 	@Test
-	public void testPersistenceAndLoadArtist() {
-		Artist artist = new Artist();
-		artist.setFirstName("John");
-		artist.setLastName("Smith");
-		artist.setId(101);
-		artistRepository.save(artist);
-		Artist oldArtist = artistRepository.findArtistById(101);
-		assertNotNull(oldArtist);
-		assertEquals(artist.getId(), oldArtist.getId());
-	}
+    public void testPersistenceAndLoadArtist() {
+        Artist artist = new Artist();
+        artist.setId(101);
+        artist.setFirstName("John");
+        artist.setLastName("Smith");
+        artist.setUsername("jsmith");
+        artist.setEmail("john.smith@mcgill.ca");
+        artist.setDescription("Hi I am a fancy artist");
+        artist.setPassword("12345");
+        artist.setPhoneNumber("5141234567");
+        artist.setBankAccountNumber("99999999");
+
+        Artwork artwork = new Artwork();
+        artwork.setId(102);
+        artwork.setArtist(artist);
+        Set<Artwork> artworkSet = new HashSet<Artwork>();
+        artworkSet.add(artwork);
+        artist.setArtwork(artworkSet);
+        
+        Transaction transaction = new Transaction();
+        transaction.setId(103);
+        transaction.setArtist(artist);
+        transaction.setArtwork(artwork);
+        Set<Transaction> transactionSet = new HashSet<Transaction>();
+        transactionSet.add(transaction);
+        artist.setTransaction(transactionSet);
+        
+        artistRepository.save(artist);
+        Artist artistQueried = artistRepository.findArtistById(101);
+        
+        assertNotNull(artistQueried);
+        assertEquals(artist.getId(), artistQueried.getId());
+        assertEquals(artist.getFirstName(), artistQueried.getFirstName());
+        assertEquals(artist.getLastName(), artistQueried.getLastName());
+        assertEquals(artist.getUsername(), artistQueried.getUsername());
+        assertEquals(artist.getEmail(), artistQueried.getEmail());
+        assertEquals(artist.getDescription(), artistQueried.getDescription());
+        assertEquals(artist.getPassword(), artistQueried.getPassword());
+        assertEquals(artist.getPhoneNumber(), artistQueried.getPhoneNumber());
+        assertEquals(artist.getBankAccountNumber(), artistQueried.getBankAccountNumber());
+        assertEquals(artist.getArtwork(), artistQueried.getArtwork());
+        assertEquals(artist.getTransaction(), artistQueried.getTransaction());
+    }
 
 	@Test
 	public void testPersistenceAndLoadCustomer() {
