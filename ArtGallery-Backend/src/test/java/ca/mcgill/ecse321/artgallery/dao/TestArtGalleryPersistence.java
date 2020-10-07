8348c795c530bfe33bf085c5e7825682b0f4b3d9
chaggysen <sen.wang@mail.mcgill.ca>
2020-10-07 16:36:45 -0400

    uncomment for justin

--- ArtGallery-Backend/src/test/java/ca/mcgill/ecse321/artgallery/dao/TestArtGalleryPersistence.java
+++ ArtGallery-Backend/src/test/java/ca/mcgill/ecse321/artgallery/dao/TestArtGalleryPersistence.java
@@ -19,7 +19,6 @@ import ca.mcgill.ecse321.artgallery.model.Artwork;
 import ca.mcgill.ecse321.artgallery.model.Picture;
 import ca.mcgill.ecse321.artgallery.model.Transaction;
 
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TestArtGalleryPersistence {
@@ -72,73 +71,74 @@ public class TestArtGalleryPersistence {
 		assertEquals("chaggy", user.getUsername());
 	}
 
-	// Other tests made by Justin
+	// Other tests
+	// made by Justin
 
-	// @Test
-	// public void testArtist() {
-	// Artist artist = new Artist();
-	// artist.setFirstName("John");
-	// artist.setLastName("Smith");
-	// artist.setId(101);
-	// artistRepository.save(artist);
-	// Artist oldArtist = artistRepository.findArtistById(101);
-	// assertNotNull(oldArtist);
-	// assertEquals(artist, oldArtist);
-	// }
+	@Test
+	public void testArtist() {
+		Artist artist = new Artist();
+		artist.setFirstName("John");
+		artist.setLastName("Smith");
+		artist.setId(101);
+		artistRepository.save(artist);
+		Artist oldArtist = artistRepository.findArtistById(101);
+		assertNotNull(oldArtist);
+		assertEquals(artist, oldArtist);
+	}
 
-	// @Test
-	// public void testCustomer() {
-	// Customer customer = new Customer();
-	// customer.setFirstName("Sidney");
-	// customer.setLastName("Crosby");
-	// customer.setId(87);
-	// customerRepository.save(customer);
-	// Customer oldCustomer = customerRepository.findCustomerById(87);
-	// assertNotNull(oldCustomer);
-	// assertEquals(customer, oldCustomer);
-	// }
+	@Test
+	public void testCustomer() {
+		Customer customer = new Customer();
+		customer.setFirstName("Sidney");
+		customer.setLastName("Crosby");
+		customer.setId(87);
+		customerRepository.save(customer);
+		Customer oldCustomer = customerRepository.findCustomerById(87);
+		assertNotNull(oldCustomer);
+		assertEquals(customer, oldCustomer);
+	}
 
-	// @Test
-	// public void testArtGallery() {
-	// ArtGallery artGallery = new ArtGallery();
-	// artGallery.setName("VanGoghEstNous");
-	// artGallery.setId(2);
-	// artGalleryRepository.save(artGallery);
-	// ArtGallery oldGallery = artGalleryRepository.findArtGalleryById(2);
-	// assertNotNull(oldGallery);
-	// assertEquals(artGallery, oldGallery);
-	// }
+	@Test
+	public void testArtGallery() {
+		ArtGallery artGallery = new ArtGallery();
+		artGallery.setName("VanGoghEstNous");
+		artGallery.setId(2);
+		artGalleryRepository.save(artGallery);
+		ArtGallery oldGallery = artGalleryRepository.findArtGalleryById(2);
+		assertNotNull(oldGallery);
+		assertEquals(artGallery, oldGallery);
+	}
 
-	// @Test
-	// public void testArtwork() {
-	// Artwork artwork = new Artwork();
-	// artwork.setName("Starry Nights");
-	// artwork.setId(3);
-	// artworkRepository.save(artwork);
-	// Artwork oldArtwork = artworkRepository.findArtworkById(3);
-	// assertNotNull(oldArtwork);
-	// assertEquals(artwork, oldArtwork);
-	// }
+	@Test
+	public void testArtwork() {
+		Artwork artwork = new Artwork();
+		artwork.setName("Starry Nights");
+		artwork.setId(3);
+		artworkRepository.save(artwork);
+		Artwork oldArtwork = artworkRepository.findArtworkById(3);
+		assertNotNull(oldArtwork);
+		assertEquals(artwork, oldArtwork);
+	}
 
-	// @Test
-	// public void testPicture() {
-	// Picture picture = new Picture();
-	// picture.setId(4);
-	// pictureRepository.save(picture);
-	// Picture oldPicture = pictureRepository.findPictureById(4);
-	// assertNotNull(oldPicture);
-	// assertEquals(picture, oldPicture);
-	// }
+	@Test
+	public void testPicture() {
+		Picture picture = new Picture();
+		picture.setId(4);
+		pictureRepository.save(picture);
+		Picture oldPicture = pictureRepository.findPictureById(4);
+		assertNotNull(oldPicture);
+		assertEquals(picture, oldPicture);
+	}
 
-	// @Test
-	// public void testTransaction() {
-	// Transaction transaction = new Transaction();
-	// transaction.setCommisionCut(0.15);
-	// transaction.setId(5);
-	// transactionRepository.save(transaction);
-	// Transaction oldTransaction = transactionRepository.findTransactionById(5);
-	// assertNotNull(oldTransaction);
-	// assertEquals(transaction, oldTransaction);
-	// }
+	@Test
+	public void testTransaction() {
+		Transaction transaction = new Transaction();
+		transaction.setCommisionCut(0.15);
+		transaction.setId(5);
+		transactionRepository.save(transaction);
+		Transaction oldTransaction = transactionRepository.findTransactionById(5);
+		assertNotNull(oldTransaction);
+		assertEquals(transaction, oldTransaction);
+	}
 
 }
