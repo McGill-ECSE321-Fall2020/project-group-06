4a5571e30b3f6450374df2edb46c5535921ea9d6
chaggysen <sen.wang@mail.mcgill.ca>
2020-10-07 12:48:59 -0400

    moved all tests tO testArtGalleryPersistence

--- ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/ArtGalleryApplication.java
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/ArtGalleryApplication.java
@@ -1,8 +1,5 @@
 package ca.mcgill.ecse321.artgallery;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ca.mcgill.ecse321.artgallery.dao.ArtGalleryRepository;
-import ca.mcgill.ecse321.artgallery.dao.ArtistRepository;
-import ca.mcgill.ecse321.artgallery.dao.ArtworkRepository;
-import ca.mcgill.ecse321.artgallery.dao.CustomerRepository;
-import ca.mcgill.ecse321.artgallery.dao.PictureRepository;
-import ca.mcgill.ecse321.artgallery.dao.TransactionRepository;
 import ca.mcgill.ecse321.artgallery.dao.UserRepository;
-import ca.mcgill.ecse321.artgallery.model.ArtGallery;
-import ca.mcgill.ecse321.artgallery.model.Artist;
-import ca.mcgill.ecse321.artgallery.model.Artwork;
-import ca.mcgill.ecse321.artgallery.model.Customer;
-import ca.mcgill.ecse321.artgallery.model.Picture;
-import ca.mcgill.ecse321.artgallery.model.Transaction;
 import ca.mcgill.ecse321.artgallery.model.User;
 
 @RestController
@@ -37,17 +22,8 @@ public class ArtGalleryApplication {
 		SpringApplication.run(ArtGalleryApplication.class, args);
 	}
 
-	// RANDOM TEST
-	// TODO
-
 	@Autowired
 	UserRepository userRepository;
-//	ArtistRepository artistRepository;
-//	CustomerRepository customerRepository;
-//	ArtworkRepository artworkRepository;
-//	PictureRepository pictureRepository;
-//	TransactionRepository transactionRepository;
-//	ArtGalleryRepository artGalleryRepository;
 
 	@RequestMapping("/")
 	public String greeting() {
@@ -62,67 +38,6 @@ public class ArtGalleryApplication {
 		userRepository.save(user);
 		return userRepository.findUserByUsername("chaggy");
 	}
-
-//	public void testArtist() {
-//		Artist artist = new Artist();
-//		artist.setFirstName("John");
-//		artist.setLastName("Smith");
-//		artist.setId(101);
-//		artistRepository.save(artist);
-//		Artist oldArtist = artistRepository.findArtistById(101);
-//		assertNotNull(oldArtist);
-//		assertEquals(artist, oldArtist);
-//	}
-//
-//	public void testCustomer() {
-//		Customer customer = new Customer();
-//		customer.setFirstName("Sidney");
-//		customer.setLastName("Crosby");
-//		customer.setId(87);
-//		customerRepository.save(customer);
-//		Customer oldCustomer = customerRepository.findCustomerById(87);
-//		assertNotNull(oldCustomer);
-//		assertEquals(customer, oldCustomer);
-//	}
-//
-//	public void testArtGallery() {
-//		ArtGallery artGallery = new ArtGallery();
-//		artGallery.setName("VanGoghEstNous");
-//		artGallery.setId(2);
-//		artGalleryRepository.save(artGallery);
-//		ArtGallery oldGallery = artGalleryRepository.findArtGalleryById(2);
-//		assertNotNull(oldGallery);
-//		assertEquals(artGallery, oldGallery);
-//	}
-//
-//	public void testArtwork() {
-//		Artwork artwork = new Artwork();
-//		artwork.setName("Starry Nights");
-//		artwork.setId(3);
-//		artworkRepository.save(artwork);
-//		Artwork oldArtwork = artworkRepository.findArtworkById(3);
-//		assertNotNull(oldArtwork);
-//		assertEquals(artwork, oldArtwork);
-//	}
-//
-//	public void testPicture() {
-//		Picture picture = new Picture();
-//		picture.setId(4);
-//		pictureRepository.save(picture);
-//		Picture oldPicture = pictureRepository.findPictureById(4);
-//		assertNotNull(oldPicture);
-//		assertEquals(picture, oldPicture);
-//	}
-//
-//	public void testTransaction() {
-//		Transaction transaction = new Transaction();
-//		transaction.setCommisionCut(0.15);
-//		transaction.setId(5);
-//		transactionRepository.save(transaction);
-//		Transaction oldTransaction = transactionRepository.findTransactionById(5);
-//		assertNotNull(oldTransaction);
-//		assertEquals(transaction, oldTransaction);
-//	}
 
 	@GetMapping("/user/{userId}") // What is this???: get a specific user by Id from the db. (Sen)
 	public ResponseEntity<Optional<User>> findUserById(@PathVariable Integer userId) {
--- ArtGallery-Backend/src/test/java/ca/mcgill/ecse321/artgallery/dao/TestArtGalleryPersistence.java
+++ ArtGallery-Backend/src/test/java/ca/mcgill/ecse321/artgallery/dao/TestArtGalleryPersistence.java
@@ -13,26 +13,52 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import ca.mcgill.ecse321.artgallery.model.User;
-
+import ca.mcgill.ecse321.artgallery.model.Artist;
+import ca.mcgill.ecse321.artgallery.model.Customer;
+import ca.mcgill.ecse321.artgallery.model.ArtGallery;
+import ca.mcgill.ecse321.artgallery.model.Artwork;
+import ca.mcgill.ecse321.artgallery.model.Picture;
+import ca.mcgill.ecse321.artgallery.model.Transaction;
 
 @RunWith(SpringRunner.class)
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TestArtGalleryPersistence {
-	
+
 	// TESTS TODO
 	// Here is a test example
 	@Autowired
 	private ArtistRepository artistRepository;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	ArtworkRepository artworkRepository;
+
+	@Autowired
+	PictureRepository pictureRepository;
+
+	@Autowired
+	TransactionRepository transactionRepository;
+
+	@Autowired
+	ArtGalleryRepository artGalleryRepository;
+
 	@AfterEach
 	public void clearDatabase() {
 		artistRepository.deleteAll();
-	}	
-	
+		userRepository.deleteAll();
+		customerRepository.deleteAll();
+		artworkRepository.deleteAll();
+		pictureRepository.deleteAll();
+		transactionRepository.deleteAll();
+		artGalleryRepository.deleteAll();
+	}
+
 	@Test
 	public void testPersistenceAndLoadUser() {
 		User user = new User();
@@ -44,5 +70,74 @@ public class TestArtGalleryPersistence {
 		assertNotNull(user);
 		assertEquals("chaggy", user.getUsername());
 	}
+
+	// Other tests made by Justin
+
+	// @Test
+	// public void testArtist() {
+	// Artist artist = new Artist();
+	// artist.setFirstName("John");
+	// artist.setLastName("Smith");
+	// artist.setId(101);
+	// artistRepository.save(artist);
+	// Artist oldArtist = artistRepository.findArtistById(101);
+	// assertNotNull(oldArtist);
+	// assertEquals(artist, oldArtist);
+	// }
+
+	// @Test
+	// public void testCustomer() {
+	// Customer customer = new Customer();
+	// customer.setFirstName("Sidney");
+	// customer.setLastName("Crosby");
+	// customer.setId(87);
+	// customerRepository.save(customer);
+	// Customer oldCustomer = customerRepository.findCustomerById(87);
+	// assertNotNull(oldCustomer);
+	// assertEquals(customer, oldCustomer);
+	// }
+
+	// @Test
+	// public void testArtGallery() {
+	// ArtGallery artGallery = new ArtGallery();
+	// artGallery.setName("VanGoghEstNous");
+	// artGallery.setId(2);
+	// artGalleryRepository.save(artGallery);
+	// ArtGallery oldGallery = artGalleryRepository.findArtGalleryById(2);
+	// assertNotNull(oldGallery);
+	// assertEquals(artGallery, oldGallery);
+	// }
+
+	// @Test
+	// public void testArtwork() {
+	// Artwork artwork = new Artwork();
+	// artwork.setName("Starry Nights");
+	// artwork.setId(3);
+	// artworkRepository.save(artwork);
+	// Artwork oldArtwork = artworkRepository.findArtworkById(3);
+	// assertNotNull(oldArtwork);
+	// assertEquals(artwork, oldArtwork);
+	// }
+
+	// @Test
+	// public void testPicture() {
+	// Picture picture = new Picture();
+	// picture.setId(4);
+	// pictureRepository.save(picture);
+	// Picture oldPicture = pictureRepository.findPictureById(4);
+	// assertNotNull(oldPicture);
+	// assertEquals(picture, oldPicture);
+	// }
+
+	// @Test
+	// public void testTransaction() {
+	// Transaction transaction = new Transaction();
+	// transaction.setCommisionCut(0.15);
+	// transaction.setId(5);
+	// transactionRepository.save(transaction);
+	// Transaction oldTransaction = transactionRepository.findTransactionById(5);
+	// assertNotNull(oldTransaction);
+	// assertEquals(transaction, oldTransaction);
+	// }
 
 }
