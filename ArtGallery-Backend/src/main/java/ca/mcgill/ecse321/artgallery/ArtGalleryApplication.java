606b41333396e9fc5eeff9afaf93711f9c490e68
chaggysen <sen.wang@mail.mcgill.ca>
2020-10-09 18:04:37 -0400

    fix imports

--- ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/ArtGalleryApplication.java
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/ArtGalleryApplication.java
@@ -1,31 +1,9 @@
 package ca.mcgill.ecse321.artgallery;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.repository.config.CustomRepositoryImplementationDetector;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import ca.mcgill.ecse321.artgallery.dao.ArtGalleryRepository;
-import ca.mcgill.ecse321.artgallery.dao.ArtistRepository;
-import ca.mcgill.ecse321.artgallery.dao.ArtworkRepository;
-import ca.mcgill.ecse321.artgallery.dao.CustomerRepository;
-import ca.mcgill.ecse321.artgallery.dao.PictureRepository;
-import ca.mcgill.ecse321.artgallery.dao.TransactionRepository;
-import ca.mcgill.ecse321.artgallery.dao.UserRepository;
-import ca.mcgill.ecse321.artgallery.model.ArtGallery;
-import ca.mcgill.ecse321.artgallery.model.Artist;
-import ca.mcgill.ecse321.artgallery.model.Artwork;
-import ca.mcgill.ecse321.artgallery.model.Customer;
-import ca.mcgill.ecse321.artgallery.model.Picture;
-import ca.mcgill.ecse321.artgallery.model.Transaction;
-import ca.mcgill.ecse321.artgallery.model.User;
 
 @RestController
 @SpringBootApplication
@@ -34,27 +12,6 @@ public class ArtGalleryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ArtGalleryApplication.class, args);
 	}
-
-	@Autowired
-	UserRepository userRepository;
-
-	@Autowired
-	CustomerRepository customerRespository;
-
-	@Autowired
-	ArtistRepository ArtistRepository;
-
-	@Autowired
-	TransactionRepository transactionRepository;
-
-	@Autowired
-	ArtGalleryRepository artGalleryRepository;
-
-	@Autowired
-	ArtworkRepository ArtworkRepository;
-
-	@Autowired
-	PictureRepository pictureRepository;
 
 	@RequestMapping("/")
 	public String greeting() {
