149c25b687bc717822452bea136ba7a6082562ca
chaggysen <sen.wang@mail.mcgill.ca>
2020-10-15 17:45:07 -0400

    implement getArtworkForSale customer service and http endpoint

--- ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/services/CustomerService.java
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/services/CustomerService.java
@@ -1,6 +1,12 @@
 package ca.mcgill.ecse321.artgallery.services;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ca.mcgill.ecse321.artgallery.dao.ArtworkRepository;
+import ca.mcgill.ecse321.artgallery.model.Artwork;
 
 /**
  * <p>
@@ -10,5 +16,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CustomerService {
+
+    @Autowired
+    ArtworkRepository artworkRepository;
+
+    /**
+     * REQ3.1: The art gallery system shall allow a customer to browse all the
+     * artworks for sale Implement the service method for this requirement
+     * 
+     * @return List of Artworks for sale
+     * @author Sen Wang
+     */
+
+    public ArrayList<Artwork> getAllArtworksForSale() {
+
+        // create new artwork for sale array list
+        ArrayList<Artwork> artworksForSale = new ArrayList<Artwork>();
+
+        // iterate through all artworks
+        for (Artwork artwork : artworkRepository.findAll()) {
+            if (artwork.isForSale()) {
+                artworksForSale.add(artwork);
+            }
+        }
+
+        return artworksForSale;
+    }
 
 }
