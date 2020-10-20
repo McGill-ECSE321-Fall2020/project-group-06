a435ed8ea86983b36ba705a670fd5c924f22c329
Andre-Walter Panzini <aw.panzini@gmail.com>
2020-10-19 20:55:55 -0400

    Implemented REQ2.1 and REQ2.2 for Service. Work in progress

--- ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/services/ArtistService.java
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/services/ArtistService.java
@@ -1,6 +1,9 @@
 package ca.mcgill.ecse321.artgallery.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ca.mcgill.ecse321.artgallery.dao.ArtworkRepository;
+import ca.mcgill.ecse321.artgallery.model.Artwork;
 
 /**
  * <p>
@@ -10,5 +13,36 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ArtistService {
-
+  
+  @Autowired
+  ArtworkRepository artworkRepository;
+  
+  /**
+   * REQ2.1 The art gallery system shall allow the artist to upload an
+   * artwork.
+   * 
+   * @param artwork the artwork to be added
+   * @return artwork the artwork that was added
+   * @author Andre-Walter Panzini
+   */
+  public Artwork uploadArtwork(Artwork artwork) {
+      artwork.setForSale(true);
+      artworkRepository.save(artwork);
+      
+      return artwork;
+  }
+  
+  /**
+   * REQ2.2 The art gallery system shall allow the artist to remove an
+   * artwork.
+   * 
+   * @param artworkId the artwork ID from the database
+   * @return artwork the artwork that was removed
+   * @author Andre-Walter Panzini
+   */
+  public Artwork removeArtwork(int artworkID) {
+      Artwork artwork = artworkRepository.findArtworkById(artworkID);
+      artworkRepository.delete(artwork);
+      return artwork;
+  }
 }
