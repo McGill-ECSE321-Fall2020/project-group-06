9b166ea837c668908bc6abe5893de20eaa0a3001
Legrandj <legrandj09@gmail.com>
2020-11-01 23:12:12 -0500

    Update AmazonClient.java

--- ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/services/AmazonClient.java
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/services/AmazonClient.java
@@ -18,7 +18,8 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
+//This is an extra service class we wrote for future implementation/front end. 
+//We therefore did not write any Service tests for it.
 @Service
 public class AmazonClient {
 
