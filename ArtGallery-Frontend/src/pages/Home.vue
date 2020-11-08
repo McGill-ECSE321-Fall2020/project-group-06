d81387c15867ee0eb933780fea663f768f21f171
chaggysen <sen.wang@mail.mcgill.ca>
2020-11-08 17:54:33 -0500

    basic frontend architecture

---
+++ ArtGallery-Frontend/src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+ <!-- Default Home Page -->
+<div>
+    <div>NavBar</div>
+    <h2>Hero component</h2>
+    <h4>Footer component</h4>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style>
+
+</style>
--- ArtGallery-Frontend/src/router/index.js
+++ ArtGallery-Frontend/src/router/index.js
@@ -1,15 +1,22 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from "vue";
+import Router from "vue-router";
+import Hello from "@/components/Hello";
+import Home from "@/pages/Home";
+import Navbar from "@/components/Navbar";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
+      path: "/",
+      name: "Hello",
       component: Hello
+    },
+    {
+      path: "/home",
+      name: "Home",
+      component: Home
     }
   ]
-})
+});
