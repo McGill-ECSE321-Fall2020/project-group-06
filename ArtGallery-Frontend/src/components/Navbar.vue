2f7f9878c846b055b6007e16367a26f5f3044c69
chaggysen <sen.wang@mail.mcgill.ca>
2020-11-09 19:35:07 -0500

    added links to navbar

--- ArtGallery-Frontend/src/components/Navbar.vue
+++ ArtGallery-Frontend/src/components/Navbar.vue
@@ -21,12 +21,14 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#"
+          <a class="nav-link" href="http://127.0.0.1:8087/#/"
             >Home <span class="sr-only">(current)</span></a
           >
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Artworks</a>
+          <a class="nav-link" href="http://127.0.0.1:8087/#/artworks"
+            >Artworks</a
+          >
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Contacts</a>
