958b860236ea151a16b6ed0637f4f7b095ce522b
chaggysen <sen.wang@mail.mcgill.ca>
2020-11-11 11:51:46 -0500

    added upload picture feature

--- ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/rest/PictureUploadRestTest.java
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/rest/PictureUploadRestTest.java
@@ -3,17 +3,20 @@ package ca.mcgill.ecse321.artgallery.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import ca.mcgill.ecse321.artgallery.dao.ArtworkRepository;
+import ca.mcgill.ecse321.artgallery.model.Artwork;
 import ca.mcgill.ecse321.artgallery.services.AmazonClient;
 
 /**
  * Art Gallery REST controller class
+ * 
  * @author Sen Wang
  * @author Noah Chamberland
  * @author Justin Legrand
@@ -22,14 +25,22 @@ import ca.mcgill.ecse321.artgallery.services.AmazonClient;
  */
 
 @RestController
-@RequestMapping("/storageTest")
+@RequestMapping("/api/storage")
 public class PictureUploadRestTest {
 
     @Autowired
     private AmazonClient amazonClient;
 
-    @PostMapping("/uploadFile")
-    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+    @Autowired
+    private ArtworkRepository artworkRepository;
+
+    @PostMapping("/uploadFile/{artworkId}")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file,
+            @PathVariable("artworkId") int artworkId) {
+        String url = amazonClient.uploadFile(file);
+        Artwork artwork = artworkRepository.findArtworkById(artworkId);
+        artwork.setUrl(url);
+        artworkRepository.save(artwork);
         return amazonClient.uploadFile(file);
     }
 
