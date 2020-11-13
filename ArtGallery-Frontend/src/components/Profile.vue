2ce6f4fadffe0f0b9365d8a35e427e5339409744
Legrandj <legrandj09@gmail.com>
2020-11-13 14:07:53 -0500

    Update Profile

--- ArtGallery-Frontend/src/components/Profile.vue
+++ ArtGallery-Frontend/src/components/Profile.vue
@@ -84,37 +84,30 @@
       </mdb-col>
       <mdb-col md="4" v-if="type == 'artist'">
         <section class="text-center pb-3">
-          <mdb-row class="d-flex justify-content-center">
+          <mdb-row
+            class="d-flex justify-content-center"
+            v-for="transac in transaction"
+            :key="transac.id"
+          >
             <mdb-col lg="6" xl="5" class="mb-3">
-              <Transaction />
-            </mdb-col>
-            <mdb-col lg="6" xl="5" class="mb-3">
-              <Transaction />
+              <Transaction v-bind:transation="transac" />
             </mdb-col>
           </mdb-row>
-          <mdb-row class="d-flex justify-content-center">
-            <mdb-col lg="6" xl="5" class="mb-3">
-              <Transaction />
-            </mdb-col>
-            <mdb-col lg="6" xl="5" class="mb-3">
-              <Transaction />
-            </mdb-col>
-            <mdb-col lg="12">
-              <div class="text-center">
-                <mdb-pagination circle color="blue">
-                  <mdb-page-item disabled>First</mdb-page-item>
-                  <mdb-page-nav prev></mdb-page-nav>
-                  <mdb-page-item active>1</mdb-page-item>
-                  <mdb-page-item>2</mdb-page-item>
-                  <mdb-page-item>3</mdb-page-item>
-                  <mdb-page-item>4</mdb-page-item>
-                  <mdb-page-item>5</mdb-page-item>
-                  <mdb-page-nav next></mdb-page-nav>
-                  <mdb-page-item disabled>Last</mdb-page-item>
-                </mdb-pagination>
-              </div>
-            </mdb-col>
-          </mdb-row>
+          <mdb-col lg="12">
+            <div class="text-center">
+              <mdb-pagination circle color="blue">
+                <mdb-page-item disabled>First</mdb-page-item>
+                <mdb-page-nav prev></mdb-page-nav>
+                <mdb-page-item active>1</mdb-page-item>
+                <mdb-page-item>2</mdb-page-item>
+                <mdb-page-item>3</mdb-page-item>
+                <mdb-page-item>4</mdb-page-item>
+                <mdb-page-item>5</mdb-page-item>
+                <mdb-page-nav next></mdb-page-nav>
+                <mdb-page-item disabled>Last</mdb-page-item>
+              </mdb-pagination>
+            </div>
+          </mdb-col>
         </section>
       </mdb-col>
       <mdb-col md="9" v-if="type == 'customer'">
@@ -260,7 +253,6 @@ export default {
     Artwork,
     Transaction,
   },
-
 };
 </script>
 
