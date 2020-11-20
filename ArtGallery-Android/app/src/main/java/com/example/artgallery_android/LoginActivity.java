2d1b4251c97f6aa3d9515c8286d1b1fee02dd301
Legrandj <legrandj09@gmail.com>
2020-11-20 00:16:16 -0500

    Start of login bar

--- ArtGallery-Android/app/src/main/java/com/example/artgallery_android/LoginActivity.java
+++ ArtGallery-Android/app/src/main/java/com/example/artgallery_android/LoginActivity.java
@@ -16,8 +16,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
