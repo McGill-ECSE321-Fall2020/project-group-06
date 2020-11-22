0e5f5225f133a7f7f87cec383850b9d8021fc09d
oli-norms <oli.norms@gmail.com>
2020-11-22 17:32:48 -0500

    Created login page

--- ArtGallery-Android/app/src/main/java/com/example/android_artgallery/MainActivity.java
+++ ArtGallery-Android/app/src/main/java/com/example/android_artgallery/MainActivity.java
@@ -3,12 +3,86 @@ package com.example.android_artgallery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refreshErrorMessage();
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        }
+
+    public void login(View v) {
+        System.out.println("Start of login method");
+        error = "";
+        final TextView tv_username = (TextView) findViewById(R.id.username);
+        final TextView tv_password = (TextView) findViewById(R.id.password);
+        System.out.println("username" + tv_username.getText().toString());
+        RequestParams loginParams = new RequestParams();
+        loginParams.put("userName", tv_username.getText().toString());
+        loginParams.put("password", tv_password.getText().toString());
+        System.out.println("Params done");
+        HttpUtils.post("api/cognito/authenticate/", loginParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                System.out.println("===================================================");
+                System.out.println(response);
+                System.out.println("===================================================");
+//                refreshErrorMessage();
+//                tv.setText("");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                System.out.println("Failure" + statusCode);
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+
     }
+
+    public void signup(View v) {}
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+
 }
