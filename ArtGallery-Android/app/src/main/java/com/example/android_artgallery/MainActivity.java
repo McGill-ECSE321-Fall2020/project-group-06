2f00542831e66c6e7402687b6851191147156090
oli-norms <oli.norms@gmail.com>
2020-11-22 23:01:22 -0500

    Login feature implementation

--- ArtGallery-Android/app/src/main/java/com/example/android_artgallery/MainActivity.java
+++ ArtGallery-Android/app/src/main/java/com/example/android_artgallery/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,10 +12,17 @@ import com.google.android.material.snackbar.Snackbar;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,35 +50,71 @@ public class MainActivity extends AppCompatActivity {
         final TextView tv_username = (TextView) findViewById(R.id.username);
         final TextView tv_password = (TextView) findViewById(R.id.password);
         System.out.println("username" + tv_username.getText().toString());
-        RequestParams loginParams = new RequestParams();
-        loginParams.put("userName", tv_username.getText().toString());
-        loginParams.put("password", tv_password.getText().toString());
+        System.out.println("password" + tv_password.getText().toString());
+//        RequestParams loginParams = new RequestParams();
+//        loginParams.put("userName", tv_username.getText().toString());
+//        loginParams.put("password", tv_password.getText().toString());
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("userName", tv_username.getText().toString());
+            jsonParams.put("password", tv_password.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        StringEntity entity = null;
+        try {
+            entity = new StringEntity(jsonParams.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         System.out.println("Params done");
-        HttpUtils.post("api/cognito/authenticate/", loginParams, new JsonHttpResponseHandler() {
+        //Context myContext = new Context();
+        HttpUtils.post(getApplicationContext(), "api/cognito/authenticate", entity, "application/json", new JsonHttpResponseHandler() {
+        //HttpUtils.post("api/cognito/authenticate", loginParams, new JsonHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            public void onSuccess(int statusCode, Header[] headers, String response /*JSONObject response*/) {
+                //System.out.println("===================================================");
+                //System.out.println(response.toString());
+                //System.out.println(response.toString());
                 System.out.println("===================================================");
-                System.out.println(response);
-                System.out.println("===================================================");
-//                refreshErrorMessage();
+                refreshErrorMessage();
 //                tv.setText("");
             }
 
+
+//            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+//                System.out.println("Failure" + statusCode);
+//                try {
+//                    error += errorResponse.get("message").toString();
+//                } catch (JSONException e) {
+//                    error += e.getMessage();
+//                }
+//                refreshErrorMessage();
+//            }
             @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+            public void onFailure(int statusCode, Header[] headers, String string, Throwable throwable) {
                 System.out.println("Failure" + statusCode);
-                try {
-                    error += errorResponse.get("message").toString();
-                } catch (JSONException e) {
-                    error += e.getMessage();
-                }
+                System.out.println("Headers" + headers);
+                System.out.println("string" + string);
+                //System.out.println("throwable" + throwable.toString());
+                refreshErrorMessage();
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject response) {
+                System.out.println("Failure" + statusCode);
+                System.out.println("Headers" + headers);
+                System.out.println("jsonObject" + response.toString());
+                //System.out.println("throwable" + throwable.toString());
                 refreshErrorMessage();
             }
         });
 
     }
 
-    public void signup(View v) {}
+    public void signup(View v) {
+        System.out.println("Start of signup method");
+
+    }
 
     private void refreshErrorMessage() {
         // set the error message
