36cfd9286e3c182609d3a3c5a35a5a89fa4d793b
oli-norms <oli.norms@gmail.com>
2020-11-25 21:38:09 -0500

    Listing of donuts

--- ArtGallery-Android/app/src/main/java/com/example/android_artgallery/Browse.java
+++ ArtGallery-Android/app/src/main/java/com/example/android_artgallery/Browse.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Browse extends AppCompatActivity {
 
@@ -12,15 +15,25 @@ public class Browse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
-    }
-    public void login (View V) {
 
-    }
-    public void signup (View V) {
+        // Create an ArrayList of Dessert objects
+        ArrayList<Dessert> desserts = new ArrayList<Dessert>();
 
+        desserts.add(new Dessert("Donut", 0, R.drawable.mona_lisa));
+        desserts.add(new Dessert("Cookie", 0, R.drawable.mona_lisa));
+        desserts.add(new Dessert("PieceOfCake", 0, R.drawable.mona_lisa));
+        desserts.add(new Dessert("Pastry", 0, R.drawable.mona_lisa));
+
+        // Create an {@link DessertAdapter}, whose data source is a list of
+        // {@link Dessert}s. The adapter knows how to create list item views for each item
+        // in the list.
+        DessertAdapter flavorAdapter = new DessertAdapter(this, desserts);
+
+        // Get a reference to the ListView, and attach the adapter to the listView.
+        ListView listView = (ListView) findViewById(R.id.listview_dessert);
+        listView.setAdapter(flavorAdapter);
     }
-    public void Browse (View V) {
-        Intent browse = new Intent(getApplicationContext(), Browse.class);
-        startActivity(browse);
+    public void browse (View V) {
+        System.out.println("Started empty browse");
     }
 }
